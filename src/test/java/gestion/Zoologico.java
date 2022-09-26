761fe39663110b35087e5b11bf847b3f7a5d3117
juanme21 <106550509+juanme21@users.noreply.github.com>
2022-09-26 14:33:15 -0500

    n

--- src/test/java/gestion/Zoologico.java
+++ src/test/java/gestion/Zoologico.java
@@ -31,4 +31,30 @@ public class Zoologico {
 	        total += zonas.get(i).cantidadAnimales();
 		return total;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public ArrayList<Zona> getZonas() {
+		return zonas;
+	}
+
+	public void setZonas(ArrayList<Zona> zonas) {
+		this.zonas = zonas;
+	}
+	
+	
 }
